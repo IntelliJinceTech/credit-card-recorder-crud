0d2be8403a9b1b36be1a9a1315214a8d6f3ece72
Christopher Jin <chrisjin1@gmail.com>
2022-07-19 22:02:03 -0400

    added mongoose and edited dbstrings

--- server.js
+++ server.js
@@ -2,55 +2,64 @@ const express = require("express");
 const app = express();
 const PORT = 8001;
 const MongoClient = require("mongodb").MongoClient;
+const mongoose = require("mongoose");
 require("dotenv").config();
+
+//add middleware
 app.set("view engine", "ejs"); //tells express that we are using EJS as template engine
+app.use(express.static("public")); //ask express to use the public folder
+app.use(express.urlencoded({ extended: true })); //help validate user input  - allows us to send arrays/objects/json
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = "credit-card-recorder",
     cardCollection;
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then((client) => {
-        console.log(`Connected to the ${dbName} database`);
-        db = client.db(dbName);
-        // cardCollection = db.collection("cards");
-    })
-    .catch((error) => console.error(error));
+mongoose.connect(dbConnectionStr, { useNewUrlParser: true }, () =>
+    console.log("Connected to db!")
+);
+
+app.listen(PORT, () => {
+    console.log(`We are live on port ${PORT}`);
+});
+
+// MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+//     .then((client) => {
+//         console.log(`Connected to the ${dbName} database`);
+//         db = client.db(dbName);
+//         cardCollection = db.collection("cards");
+//     })
+//     .catch((error) => console.error(error));
 
 // app.get("/", (req, res) => {
 //     res.sendFile(__dirname + "/index.html");
 // });
 
-app.get("/", (request, response) => {
-    db.collection("cards")
-        .find()
-        .sort({ points: -1 })
-        .toArray()
-        .then((data) => {
-            response.render("index.ejs", { info: data });
-        })
-        .catch((error) => console.error(error));
-});
+// app.get("/", (request, response) => {
+//     cardCollection
+//         .find()
+//         .sort({ points: -1 })
+//         .toArray()
+//         .then((data) => {
+//             response.render("index.ejs", { info: data });
+//         })
+//         .catch((error) => console.error(error));
+// });
 
-app.post("/creditcards", (request, response) => {
-    db.collection("cards")
-        .insertOne({
-            cardIssuer: request.body.cardIssuer,
-            cardName: request.body.cardName,
-            points: 0,
-        })
-        .then((result) => {
-            console.log("Rapper Added");
-            response.redirect("/");
-        })
-        .catch((error) => console.error(error));
-});
+// app.post("/creditcards", (request, response) => {
+//     cardCollection
+//         .insertOne({
+//             cardIssuer: request.body.cardIssuer,
+//             cardName: request.body.cardName,
+//             points: 0,
+//         })
+//         .then((result) => {
+//             console.log("Credit Card Added");
+//             response.redirect("/");
+//         })
+//         .catch((error) => console.error(error));
+// });
 
 // app.post("/creditcards", (req, res) => {
 //     console.log("swipe me bro");
 // });
-
-app.listen(PORT, () => {
-    console.log(`We are live on port ${PORT}`);
-});
